83909d58696042fb6ce2c71ea7e04761fedbc19d
Juan Araujo <araujogreydiego@gmail.com>
2021-06-17 18:25:50 -0400

    Adds the logic to calculate the remaining and spent so far on the budget

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 export default function ExpenseTotal() {
+  const { expenses } = useContext(AppContext);
+  // Use the reduce function to get a total of all the costs and assign this to the variable
+  const spent = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
   return (
     <div className="alert alert-primary">
-      <span>Spent so far: $800</span>
+      <span>Spent so far: ${spent}</span>
     </div>
-  )
+  );
 }
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 export default function Remaining() {
+  const { expenses, budget } = useContext(AppContext);
+  // With reduce we bring all the cost and sum them to total.
+  const totalSpent = expenses.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+
+  // We calculate based on overspending the color of the class
+  const alertType = totalSpent > budget ? "alert-danger" : "alert-success";
+
   return (
     <div>
-      <div className="alert alert-success">
-        <span>Remaining: $2800</span>
+      <div className={`alert ${alertType}`}>
+      {/* Here we do the math subtracting the total that has been spent to the budget amount */}
+        <span>Remaining: ${budget - totalSpent}</span>
       </div>
     </div>
-  )
+  );
 }
-
