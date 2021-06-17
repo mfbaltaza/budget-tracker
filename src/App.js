d9a4982580e38c95f355158dba91f31cc3d3a29e
Juan Araujo <araujogreydiego@gmail.com>
2021-06-17 16:30:06 -0400

    Adds Basic Expense Form Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Budget from "./components/Budget"
-import Remaining from "./components/Remaining"
-import ExpenseTotal from "./components/ExpenseTotal"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
 
 // We are mainly using React over className
 
 export default function App() {
   return (
-    <div className="Container">
+    <div className="container">
       <h1 className="mt-3">An Awesome Budget Planner</h1>
       {/* Below we add the main 3 elements in a row */}
       <div className="row mt-3">
@@ -21,7 +23,18 @@ export default function App() {
         <div className="col-sm">
           <ExpenseTotal />
         </div>
+        <h3 className="mt-3">Expenses</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AddExpenseForm />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
---
+++ src/components/AddExpenseForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function AddExpenseForm() {
+  return (
+    <form>
+      {/* Section for the Add Expenses Area */}
+      <div className="row">
+        <div className="col-sm">
+          <label htmlFor="name">Name</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="name"
+          ></input>
+        </div>
+        <div className="col-sm">
+          <label htmlFor="cost">Cost</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="name"
+          ></input>
+        </div>
+      </div>
+      <h3>Add Expense</h3>
+      <div className="row">
+        <div className="col-sm">
+          <button type="submit" className="btn btn-primary mt3">
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
